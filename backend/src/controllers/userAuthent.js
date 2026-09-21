390715492cb0802867a6033e43ba98c09bbf6078
Naveen kumar <160026999+naveenpachahara@users.noreply.github.com>
2026-09-22 03:13:16 +0530

    Enhance cookie security settings for user authentication

--- backend/src/controllers/userAuthent.js
+++ backend/src/controllers/userAuthent.js
@@ -27,7 +27,7 @@ const register = async (req,res)=>{
         role:user.role,
     }
     
-     res.cookie('token',token,{maxAge: 60*60*1000});
+     res.cookie('token',token,{maxAge: 60*60*1000, httpOnly: true, secure: true, sameSite: 'none'});
      res.status(201).json({
         user:reply,
         message:"Loggin Successfully"
@@ -51,6 +51,9 @@ const login = async (req,res)=>{
 
         const user = await User.findOne({emailId});
 
+        if(!user)
+            throw new Error("Invalid Credentials");
+
         const match = await bcrypt.compare(password,user.password);
 
         if(!match)
@@ -64,7 +67,7 @@ const login = async (req,res)=>{
         }
 
         const token =  jwt.sign({_id:user._id , emailId:emailId, role:user.role},process.env.JWT_KEY,{expiresIn: 60*60});
-        res.cookie('token',token,{maxAge: 60*60*1000});
+        res.cookie('token',token,{maxAge: 60*60*1000, httpOnly: true, secure: true, sameSite: 'none'});
         res.status(201).json({
             user:reply,
             message:"Loggin Successfully"
@@ -90,7 +93,7 @@ const logout = async(req,res)=>{
     //    Token add kar dung Redis ke blockList
     //    Cookies ko clear kar dena.....
 
-    res.cookie("token",null,{expires: new Date(Date.now())});
+    res.cookie("token",null,{expires: new Date(Date.now()), httpOnly: true, secure: true, sameSite: 'none'});
     res.send("Logged Out Succesfully");
 
     }
@@ -113,7 +116,7 @@ const adminRegister = async(req,res)=>{
     
      const user =  await User.create(req.body);
      const token =  jwt.sign({_id:user._id , emailId:emailId, role:user.role},process.env.JWT_KEY,{expiresIn: 60*60});
-     res.cookie('token',token,{maxAge: 60*60*1000});
+     res.cookie('token',token,{maxAge: 60*60*1000, httpOnly: true, secure: true, sameSite: 'none'});
      res.status(201).send("User Registered Successfully");
     }
     catch(err){
